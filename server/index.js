05ee9e52ade8dc89ecdb5497632b8cffaa1681c3
piyush52dixit <piyushdixit567@gmail.com>
2024-02-22 14:10:34 +0530

    Included Mui Using it's Card Component

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 const postsRouter = require("./routes/Posts");
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+
 const PORT = 5555;
 
 const db = require("./models/index");
